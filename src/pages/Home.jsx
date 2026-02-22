ba7b5357d519dcf2ea91e036b3f0ad2ebed885cb
Edward Dee <dee422@163.com>
2026-02-23 02:11:39 +0800

    Initial React web setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "80px 40px" }}>
+      <h1>Hearth Studio</h1>
+      <p>Custom engraved porcelain that glows with light.</p>
+    </div>
+  );
+}
